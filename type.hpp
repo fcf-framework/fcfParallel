35e421720df7d310c0a544d8ddabfc1602b94b3b
Vladimir Markin <v.o.markin221@gmail.com>
2025-01-08 01:28:59 +0300

    Update type.hpp

--- type.hpp
+++ type.hpp
@@ -1,44 +1,71 @@
 #ifndef ___FCF__PARALLEL__TYPE_HPP___
 #define ___FCF__PARALLEL__TYPE_HPP___
+
+
   #ifndef FCF_INT8_TYPE
+    #define FCF_INT8_TYPE_INDEX 1
     #define FCF_INT8_TYPE fcf_int8
     typedef char fcf_int8;
   #endif // #ifndef FCF_INT8_TYPE
+
   #ifndef FCF_UINT8_TYPE
+    #define FCF_UINT8_TYPE_INDEX 2
     #define FCF_UINT8_TYPE fcf_uint8
     typedef unsigned char fcf_uint8;
   #endif // #ifndef FCF_UINT8_TYPE
+
   #ifndef FCF_INT16_TYPE
+    #define FCF_INT16_TYPE_INDEX 3
     #define FCF_INT16_TYPE fcf_int16
     typedef short fcf_int16;
   #endif // #ifndef FCF_INT16_TYPE
+
   #ifndef FCF_UINT16_TYPE
+    #define FCF_UINT16_TYPE_INDEX 4
     #define FCF_UINT16_TYPE fcf_uint16
     typedef unsigned short fcf_uint16;
   #endif // #ifndef FCF_UINT16_TYPE
+
   #ifndef FCF_INT32_TYPE
+    #define FCF_INT32_TYPE_INDEX 5
     #define FCF_INT32_TYPE fcf_int32
     typedef int fcf_int32;
   #endif // #ifndef FCF_INT32_TYPE
+
   #ifndef FCF_UINT32_TYPE
+    #define FCF_UINT32_TYPE_INDEX 6
     #define FCF_UINT32_TYPE fcf_uint32
     typedef unsigned int fcf_uint32;
   #endif // #ifndef FCF_UINT32_TYPE
+
   #ifndef FCF_INT64_TYPE
+    #define FCF_INT64_TYPE_INDEX 7
     #define FCF_INT64_TYPE fcf_int64
     typedef long long fcf_int64;
   #endif // #ifndef FCF_INT64_TYPE
+
   #ifndef FCF_UINT64_TYPE
+    #define FCF_UINT64_TYPE_INDEX 8
     #define FCF_UINT64_TYPE fcf_uint64
     typedef unsigned long long fcf_uint64;
   #endif // #ifndef FCF_UINT64_TYPE
+
   #ifndef FCF_FLOAT32_TYPE
+    #define FCF_FLOAT32_TYPE_INDEX 9
     #define FCF_FLOAT32_TYPE fcf_float32
     typedef float fcf_float32;
   #endif // #ifndef FCF_FLOAT32_TYPE
+
   #ifndef FCF_FLOAT64_TYPE
+    #define FCF_FLOAT64_TYPE_INDEX 10
     #define FCF_FLOAT64_TYPE fcf_float64
     typedef double fcf_float64;
   #endif // #ifndef FCF_FLOAT64_TYPE
+
+  #ifndef FCF_FLOAT80_TYPE
+    #define FCF_FLOAT80_TYPE_INDEX 11
+    #define FCF_FLOAT80_TYPE fcf_float80
+    typedef long double fcf_float80;
+  #endif // #ifndef FCF_FLOAT80_TYPE
 
 #endif // #ifndef ___FCF__PARALLEL__TYPE_HPP___
